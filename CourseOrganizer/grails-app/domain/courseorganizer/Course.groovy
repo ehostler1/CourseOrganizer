420445e7cc8dcfc1f4b965e91f22c2396d1c3f72
Ethan Hostler <ehostler1@ycp.edu>
2019-03-28 16:55:48 -0400

    Course domain model added

---
+++ CourseOrganizer/grails-app/domain/courseorganizer/Course.groovy
@@ -0,0 +1,20 @@
+package courseorganizer
+
+class Course {
+	
+	String crn
+	String subject
+	String course
+	String section
+	String title
+	String days
+	String beginTime
+	String endTime
+	String instructor
+	String beginDay
+	String endDay
+	String location
+
+    static constraints = {
+    }
+}
